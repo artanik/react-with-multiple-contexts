ec920b8c37544646df058cf84ff10f508d0df5fd
Artem Anikeev <artanik@users.noreply.github.com>
2019-02-02 19:50:28 +0200

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -5,14 +5,14 @@ import { withContextConsumer, withContextProvider } from '.';
 export const context = React.createContext({});
 
 class Component extends React.Component {
-	render() {
-		return React.createElement('div', null, this.props.children);
-	}
+  render() {
+    return React.createElement('div', null, this.props.children);
+  }
 }
 
 const DummyComponent = () => React.createElement('div');
 
 expectType<React.ReactNode>(withContextConsumer(Component, { context }));
 expectType<React.ReactNode>(withContextProvider(DummyComponent, ({ value }) => ([
-	{ context, value }
+  { context, value }
 ])));
